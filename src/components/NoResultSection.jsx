c97a9e96c73b6cebe755740e41103c2c782361fd
Rutu-yadav <yadavrutuja65@gmail.com>
2025-05-25 18:09:01 +0530

    update

--- src/components/NoResultSection.jsx
+++ src/components/NoResultSection.jsx
@@ -7,7 +7,7 @@ function NoResultSection() {
       <h3 className="text-center text-xl py-4 ">Something went wrong</h3>
       <p className="text-center text-base">
         We&apos;re unable to retrive the weather details.Ensure you&apos;ve
-        enterec a valid city or try again later.
+        entered a valid city or try again later.
       </p>
     </div>
   );
